a906ec28418f6a3d086b97c848d341730ebe99f3
joel moore <b4ir2old@proton.me>
2025-01-12 15:06:40 -0500

    Create CSC284_classList.py
    Class list program

---
+++ CSC284_classList.py
@@ -0,0 +1,26 @@
+# joel moore    spring 2025     CSC284_classListS.py
+
+'''
+This program is modified by each CSC284 student in the following way:
+
+1. adding their name to the list below
+2. printing the list
+3. update the program in HJMoore11/CSC284
+
+'''
+# ============================define list, lastName, firstName
+
+CSC284_2025SP=[
+    'Moore, Joel',
+    'Student, Test'
+]
+
+# ============================sort the list
+
+CSC284_2025SP.sort()
+
+# =====================================print the list
+
+print('2025SP CSC284 Alpahbetical Class List ')
+for name in CSC284_2025SP:
+    print(f'  {name}')
